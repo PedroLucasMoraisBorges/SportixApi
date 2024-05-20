bd3d8e2b9b07251288c1fc3adaa1ddbbf1ecafd5
Daniel Tavares Almeida Correia <daniel.tavares@aluno.unifapce.edu.br>
2024-05-20 18:01:51 -0300

    feat(user) - edit info

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,11 +3,17 @@ import { PrismaService } from 'src/database/prisma.service';
 import { randomUUID } from 'crypto';
 import * as bcrypt from 'bcrypt'
 import { CreateUserBody } from './dtos/create-user-body';
+import { UserEdit, UserLogin } from './entities/user.entity';
+import { EditUserBody } from './dtos/edit-user-body';
 
 @Injectable()
 export class UserService {
   constructor(private readonly prisma: PrismaService) { }
+<<<<<<< Updated upstream
   async create(object: CreateUserBody) {    
+=======
+  async create(object: CreateUserBody) {
+>>>>>>> Stashed changes
 
     const email = object.email
 
@@ -30,7 +36,7 @@ export class UserService {
         cpf: object.cpf,
         email: object.email,
         password: passwordHash,
-        phoneNumber : object.phoneNumber
+        phoneNumber: object.phoneNumber
       }
     })
 
@@ -41,7 +47,7 @@ export class UserService {
 
   async findByEmail(email: string) {
     return await this.prisma.user.findUnique({
-        where: { email }
+      where: { email }
     });
   }
 
@@ -49,4 +55,38 @@ export class UserService {
     const users = await this.prisma.user.findMany();
     return users.map(user => user.name);
   }
+<<<<<<< Updated upstream
+=======
+
+  async getUserInfo(user: UserLogin) {
+    const userInfo = {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      cpf: user.cpf,
+      phoneNumber: user.phoneNumber,
+      court: user.court
+    };
+    return userInfo;
+  }
+
+  async editUser(editUserBody : EditUserBody, user : UserLogin) {
+    const {name, cpf, phoneNumber} = editUserBody
+
+    const data : UserEdit = {};
+
+    if (name !== undefined && name !== null) data.name = name;
+    if (cpf !== undefined && cpf !== null) data.cpf = cpf;
+    if (phoneNumber !== undefined && phoneNumber !== null) data.phoneNumber = phoneNumber;
+
+    const updatedUser = await this.prisma.court.update({
+      where: {
+        id: user.id
+      },
+      data: data
+    });
+
+    return updatedUser;
+  }
+>>>>>>> Stashed changes
 }
